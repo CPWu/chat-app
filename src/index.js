2521ab51534342eb38b03a96e85400fab0a9c686
Chun Wu <the.chun.wu@gmail.com>
2020-02-09 21:18:11 -0500

    Beginning of a simple chat-app with socket.io

--- src/index.js
+++ src/index.js
@@ -8,3 +8,19 @@
  * Created: February 09, 2020
  * 
  */
+
+//
+const path = require('path')
+const express = require('express')
+const app = express()
+// custom modules
+
+// variables
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+app.listen(port, () => {
+    console.log(`Server is up on port: ${port}`)
+})
